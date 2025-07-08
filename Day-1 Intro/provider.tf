0c4deac91ce276ee31c8dcd68fddd043389e727b
TaosifAhmed88 <toasifahmed2003@gmail.com>
2025-07-08 20:26:22 +0530

    Day-1 Modified

--- Day-1 Intro/provider.tf
+++ Day-1 Intro/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   
 }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.100.0"
+    }
+  }
+}
+#You need to upgrade the terraform when you are changing the version of providers   --> terraform init -upgrade
